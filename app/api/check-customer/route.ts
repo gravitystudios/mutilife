e34e0d234f32fad5e9fc5f56eadba2acd7329313
Admin <admin@mutilife.co.za>
2026-03-05 10:00:59 +0200

    Boolean for Endpoint

--- app/api/check-customer/route.ts
+++ app/api/check-customer/route.ts
@@ -28,7 +28,8 @@ export async function POST(request: Request) {
       return NextResponse.json({
         name: lastOrder.customer_name,
         number: lastOrder.customer_phone,
-        duration: duration
+        duration: duration,
+        reorder: false
       })
     }
 
